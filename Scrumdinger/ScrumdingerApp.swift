5db48d85992586dfa5f20f0cd903610aa1b808f5
Ashley Nygaard <ashley.d.nygaard@gmail.com>
2023-03-21 12:10:36 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ashley Nygaard on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
